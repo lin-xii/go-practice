9bc0ca0b11d050965688bdee8deea4389001b016
lin.xii <lin.xii@outlook.com>
2023-08-15 16:34:02 +0800

    values: multi base type

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Values() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1:", 1+1)
+	fmt.Println("7.0/3.0:", 7.0/3)
+	fmt.Println("7/3:", 7/3)
+	fmt.Println(true && false)
+}
