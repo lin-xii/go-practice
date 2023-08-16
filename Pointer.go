8708ac70367437c9d67e0211f3d83958fb75a8c8
lin.xii <lin.xii@outlook.com>
2023-08-16 17:10:36 +0800

    struct: &结构体。自动解引用，增加了理解的难度

--- Pointer.go
+++ Pointer.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type persion struct {
+	name string
+	age  int
+}
+
 func Pointer() {
 	var a int = 1
 	fmt.Println(a, &a, *(&a))
@@ -13,4 +18,14 @@ func Pointer() {
 	fmt.Println(&b)
 	// 其实，就是*了两次，才拿到的值
 	fmt.Println(**(&b))
+
+	fmt.Println("--------------------")
+	person1 := persion{"name1", 21}
+	fmt.Println("person1", person1)
+
+	person2 := &person1
+	fmt.Println("person2:", person2)
+	fmt.Println(*&person2.name)
+	fmt.Println((*person2).name)
+	fmt.Println(*&person2.age)
 }
