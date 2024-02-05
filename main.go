9ad00fda94a4bf1b8a4d7f61ed0d582f876db7c8
lin.guo <lin.guo@chaitin.com>
2024-02-05 16:15:27 +0800

    feat: hello

--- main.go
+++ main.go
@@ -1,76 +1,8 @@
 package main
 
-import (
-	_ "github.com/lin-xii/go-example/niuke"
-	_ "github.com/lin-xii/go-example/niuke/control"
-	_ "github.com/lin-xii/go-example/niuke/errors"
-	_ "github.com/lin-xii/go-example/niuke/function"
-	_ "github.com/lin-xii/go-example/niuke/interfaces"
-	_ "github.com/lin-xii/go-example/niuke/loop"
-	_ "github.com/lin-xii/go-example/niuke/maps"
-	_ "github.com/lin-xii/go-example/niuke/structs"
-
-	_ "github.com/lin-xii/go-example/inaction/chapter4"
-	_ "github.com/lin-xii/go-example/inaction/chapter5"
-	_ "github.com/lin-xii/go-example/inaction/one"
-	_ "github.com/lin-xii/go-example/practice"
-)
+import "fmt"
 
 // 一些练习
 func main() {
-	// Helloworld()
-	// Values()
-	// Variables()
-	// For()
-	// If()
-	// Switch()
-	// Pointer()
-	// Goroutine()
-	// Channel()
-	// Rune()
-	// niuke.ExecCountString()
-	// niuke.ExecIsPalindrome()
-	// niuke.ExecDivision()
-	// niuke.ExecCompare()
-	// niuke.ExecLogicOperation()
-	// niuke.ExecBitOperation()
-	// niuke.ExecOdevity()
-	// niuke.ExecArray()
-	// niuke.ExecMakeSlice()
-	// niuke.ExecSliceCopy()
-	// niuke.ExecDeleteElement()
-	// niuke.ExecMinAndMax()
-	// niuke.ExecConvert()
-	// niuke.ExecEqual()
-	// niuke.ExecMerge()
-	// niuke.ExecWardrobe()
-	// maps.ExecScore()
-	// maps.ExecCharacter()
-	// maps.ExecCanConstruct()
-	// maps.ExecGetNoRepeat()
-	// control.ExecGetAge()
-	// control.ExecJudgeScore()
-	// control.ExecIspay()
-	// control.ExecPushbox()
-	// loop.ExecMultiplication()
-	// loop.ExecTransform()
-	// loop.ExecCheck()
-	// loop.ExecCanPass()
-	// function.ExecFactorial()
-	// function.ExecAbsfunc()
-	// function.ExecOperate()
-	// structs.Students2()
-	// interfaces.Animals()
-	// errors.ExecTemprature()
-	// practice.RunMultiReturns()
-	// one.RunInterface()
-	// chapter4.Recuperate()
-	// chapter4.NilOrBlankSlice()
-	// chapter4.BottomArray()
-	// chapter4.SomeArray()
-	// chapter4.SomeMap()
-	// chapter5.BasicTypeDeclare()
-	// chapter5.ComplexTypeDeclare()
-	// chapter5.ValueDelivery()
-	println("hello, 2024")
+	fmt.Println("hello go, in 2024")
 }
