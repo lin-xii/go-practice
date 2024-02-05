950bba3f06bd9db18183a083eafbd119e49103a4
lin.guo <lin.guo@chaitin.com>
2024-02-05 18:13:56 +0800

    int区分unsign, sign。有符号数，少1bit，用作正负

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import "fmt"
 
 // 一些练习
 func main() {
-	fmt.Println(addition(1, "2"))
+	fmt.Println(addition(1, 2))
 }
 
 func addition(a, b int) int {
 	return a + b
+}
+
+func primitiveType() {
+	var aInt8 int8 = 128
+	var aUint8 uint8 = 256
+	var aFloat float32 = 1.1
+	var aString string = "hello"
+	var aBool bool = true
 }
