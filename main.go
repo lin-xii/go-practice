825d1984fcec888507d205d9b3853b1d225f6d13
lin.guo <lin.guo@chaitin.com>
2024-02-05 18:23:09 +0800

    小心具体int类型的范围。int8、uint8要小心

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func addition(a, b int) int {
 }
 
 func primitiveType() {
-	var aInt8 int8 = 128
-	var aUint8 uint8 = 256
+	var aInt int = 2 ^ 32 + 1
+	// 0 ~ 127
+	var aInt8 int8 = 127
+	// -256 ~ 255
+	var aUint8 uint8 = 255
 	var aFloat float32 = 1.1
 	var aString string = "hello"
 	var aBool bool = true
+
+	fmt.Println(aInt, aInt8, aUint8, aFloat, aString, aBool)
 }
